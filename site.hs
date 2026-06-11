56abcf3d551900b5e16fd2532f3051114a2a120b
Brandon Saint-John <branstj@gmail.com>
2026-06-10 22:29:04 -0700

    feat: support cv
    Signed-off-by: Brandon Saint-John <branstj@gmail.com>

--- site.hs
+++ site.hs
@@ -1,6 +1,7 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import           System.FilePath (takeFileName)
 import           Hakyll
 
 
@@ -9,6 +10,10 @@ main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
         route   idRoute
+        compile copyFileCompiler
+
+    match "resume-cv/cv.pdf" $ do
+        route   $ customRoute (takeFileName . toFilePath)
         compile copyFileCompiler
 
     match "css/*" $ do
