fa4c8b7ab4948b4a985789d9f670a454a785deb9
Brandon Saint-John <branstj@gmail.com>
2026-06-21 20:17:31 -0700

    feat: a /uses page
    Signed-off-by: Brandon Saint-John <branstj@gmail.com>

--- site.hs
+++ site.hs
@@ -25,6 +25,12 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" aboutCtx
             >>= relativizeUrls
 
+    match "uses.markdown" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
---
+++ uses.markdown
@@ -0,0 +1,17 @@
+---
+title: Uses
+---
+
+A running list of the hardware, software, and tools I use day to day.
+
+## Editor & Terminal
+
+- TODO
+
+## Development
+
+- TODO
+
+## Hardware
+
+- TODO
