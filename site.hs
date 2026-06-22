f7144800b5a92e864513f039a795a022a89cd870
Brandon Saint-John <branstj@gmail.com>
2026-06-21 20:16:18 -0700

    feat: remove Contact page
    Signed-off-by: Brandon Saint-John <branstj@gmail.com>

--- site.hs
+++ site.hs
@@ -25,12 +25,6 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" aboutCtx
             >>= relativizeUrls
 
-    match "contact.markdown" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
